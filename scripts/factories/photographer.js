b947cd3f073f31946afc1fdd29c184e77371474d
AndyRama <andyramaroson@gmail.com>
2022-04-17 18:04:20 +0200

    [enhancement] fecth data.json and slice content index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+const { name, portrait, city, country, tagline, price, id } = data;
+const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+  function getUserCardDOM() {
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    img.setAttribute("src", picture);
+    const h2 = document.createElement('h2');
+    h2.textContent = name;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    //add city & country
+    const hCity = document.createElement('h3');
+    hCity.classList.add('city');
+    hCity.textContent = `${city}, ${country}`; 
+
+    //Add tagline
+    const ptagline = document.createElement('p');
+    ptagline.classList.add('tagline');
+    ptagline.innerHTML = tagline;
+
+    //add price 
+    const tjmprice = document.createElement('span');
+    ptagline.classList.add('price');
+    tjmprice.innerHTML=`${price}/par jours`;
+
+    article.appendChild(img);
+    
+    article.appendChild(h2);
+    article.appendChild(hCity);
+    article.appendChild(ptagline);
+    article.appendChild(tjmprice);
+
+    return (article);
+  }
+  return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,1 @@
 //Mettre le code JavaScript lié à la page photographer.html
-function photographerFactory(data){
-  const{ name, portrait, city, country, tagline } = data;
-  
-} 
